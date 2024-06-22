b834f6a8033c80eb211ad312af7ac8ff5361f3bf
Grisha <grisha765@tuta.io>
2024-06-23 01:40:41 +0300

    [u] notify

--- hyprland/record_screen.sh
+++ hyprland/record_screen.sh
@@ -8,9 +8,8 @@ if pgrep -x "wf-recorder" > /dev/null
 then
     # Если wf-recorder уже запущен, останавливаем запись
     pkill -x "wf-recorder"
-    notify-send "Запись экрана остановлена"
+    notify-send "Запись экрана остановлена и сохранена в: $output_file"
 else
     # Если wf-recorder не запущен, начинаем запись
     wf-recorder -f "$output_file" -g "$(slurp -d)" &
-    notify-send "Запись экрана начата: $output_file"
 fi
