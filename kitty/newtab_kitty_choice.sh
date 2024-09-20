71c30390d2c9f87ac62ecf87267abc17d0179079
Grisha <grisha765@tuta.io>
2024-09-20 19:49:00 +0300

    [u] add custom sessions

--- kitty/newtab_kitty_choice.sh
+++ kitty/newtab_kitty_choice.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 
+session_file="$HOME/sessions.conf"
+sessions=""
+
+if [ -f "$session_file" ]; then
+    while IFS= read -r line; do
+        if [[ $line =~ ^\"(.*)\"\ \"(.*)\"$ ]]; then
+            name="${BASH_REMATCH[1]}"
+            command="${BASH_REMATCH[2]}"
+            sessions+="$name\n"
+        fi
+    done < "$session_file"
+fi
+
 containers=$(echo -e "Host\n$(distrobox list | tail -n +2 | awk -F'|' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//')")
 
+if [ -n "$sessions" ]; then
+    containers=$(echo -e "$containers\n$sessions")
+fi
+
 if [ -z "$containers" ]; then
-    echo "No containers available."
+    echo "No containers or sessions available."
     exit 1
 fi
 
-selected_container=$(echo "$containers" | fzf --prompt="Select a container or Host: ")
+selected_container=$(echo -e "$containers" | fzf --prompt="Select a container or Host: ")
 
 if [ "$selected_container" == "Host" ]; then
     kitty @ set-tab-title "Host"
     exec bash
-
-elif [ -n "$selected_container" ]; then
-    kitty @ set-tab-title "$selected_container"
-    distrobox enter "$selected_container"
+elif [[ "$selected_container" && "$selected_container" != "Host" ]]; then
+    if [ -f "$session_file" ] && grep -q "\"$selected_container\"" "$session_file"; then
+        command=$(grep "\"$selected_container\"" "$session_file" | sed -E 's/.*"([^"]+)"/\1/')
+        kitty @ set-tab-title "$selected_container"
+        eval "$command"
+    else
+        kitty @ set-tab-title "$selected_container"
+        distrobox enter "$selected_container"
+    fi
 else
-    echo "No container selected."
+    echo "No container or session selected."
 fi
 
