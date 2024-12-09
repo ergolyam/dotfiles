1f42482fc9f184cb54be0e951a8f8b0fa8a33700
Grisha <grisha765@tuta.io>
2024-12-10 00:25:21 +0300

    [-] cleanup

--- hyprland/record_screen.sh
+++ hyprland/record_screen.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Путь к файлу для записи
 output_file="$HOME/Videos/Записи экрана/Запись экрана от $(date +'%Y-%m-%d %H-%M-%S').mkv"
 
-# Проверяем, запущен ли wf-recorder
 if pgrep -x "wf-recorder" > /dev/null
 then
-    # Если wf-recorder уже запущен, останавливаем запись
     pkill -x "wf-recorder"
     notify-send "Запись экрана остановлена и сохранена в: $output_file"
 else
-    # Если wf-recorder не запущен, начинаем запись
     notify-send "Запись экрана начата"
     wf-recorder -f "$output_file" -g "$(slurp -d)" &
 fi
