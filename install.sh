aea7ef2ec1b4e17f2797c036110e3c22ee5e71f9
Grisha <grisha765@tuta.io>
2024-12-19 21:06:08 +0300

    [u] use basedir

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$(realpath "$BASH_SOURCE")")
+
+echo "Script dir: $SCRIPT_DIR"
+
 # Создание необходимых директорий, если они не существуют
 echo "Create dir's."
 mkdir -p ~/.config/hypr
@@ -14,51 +18,51 @@ mkdir -p ~/.config/opensd/profiles
 # Создание символических ссылок для файлов и директорий
 
 echo "Create symlinks for hyprland."
-ln -sf ~/dotfiles/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sf ~/dotfiles/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
-ln -sf ~/dotfiles/hyprland/wallapaper.png ~/.config/hypr/wallapaper.png
-ln -sf ~/dotfiles/hyprland/keys.conf ~/.config/hypr/keys.conf
-ln -sf ~/dotfiles/hyprland/monitors.conf ~/.config/hypr/monitors.conf
-ln -sf ~/dotfiles/hyprland/startup.conf ~/.config/hypr/startup.conf
-ln -sf ~/dotfiles/hyprland/wm.conf ~/.config/hypr/wm.conf
-ln -sf ~/dotfiles/hyprland/wmrules.conf ~/.config/hypr/wmrules.conf
-ln -sf ~/dotfiles/hyprland/record_screen.sh ~/.config/hypr/record_screen.sh
-ln -sf ~/dotfiles/hyprland/monitors_select.sh ~/.config/hypr/monitors_select.sh
-ln -sf ~/dotfiles/hyprland/set_workspace.sh ~/.config/hypr/set_workspace.sh
-ln -sf ~/dotfiles/hyprland/toggle_overview_mode.sh ~/.config/hypr/toggle_overview_mode.sh
-ln -sf ~/dotfiles/hyprland/set_scroller_mode.sh ~/.config/hypr/set_scroller_mode.sh
+ln -sf $SCRIPT_DIR/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sf $SCRIPT_DIR/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+ln -sf $SCRIPT_DIR/hyprland/wallapaper.png ~/.config/hypr/wallapaper.png
+ln -sf $SCRIPT_DIR/hyprland/keys.conf ~/.config/hypr/keys.conf
+ln -sf $SCRIPT_DIR/hyprland/monitors.conf ~/.config/hypr/monitors.conf
+ln -sf $SCRIPT_DIR/hyprland/startup.conf ~/.config/hypr/startup.conf
+ln -sf $SCRIPT_DIR/hyprland/wm.conf ~/.config/hypr/wm.conf
+ln -sf $SCRIPT_DIR/hyprland/wmrules.conf ~/.config/hypr/wmrules.conf
+ln -sf $SCRIPT_DIR/hyprland/record_screen.sh ~/.config/hypr/record_screen.sh
+ln -sf $SCRIPT_DIR/hyprland/monitors_select.sh ~/.config/hypr/monitors_select.sh
+ln -sf $SCRIPT_DIR/hyprland/set_workspace.sh ~/.config/hypr/set_workspace.sh
+ln -sf $SCRIPT_DIR/hyprland/toggle_overview_mode.sh ~/.config/hypr/toggle_overview_mode.sh
+ln -sf $SCRIPT_DIR/hyprland/set_scroller_mode.sh ~/.config/hypr/set_scroller_mode.sh
 
 echo "Create symlinks for waybar."
-ln -sf ~/dotfiles/waybar/config ~/.config/waybar/config
-ln -sf ~/dotfiles/waybar/ethernet.sh ~/.config/waybar/ethernet.sh
-ln -sf ~/dotfiles/waybar/modules ~/.config/waybar/modules
-ln -sf ~/dotfiles/waybar/style.css ~/.config/waybar/style.css
-ln -sf ~/dotfiles/waybar/wifi.sh ~/.config/waybar/wifi.sh
-ln -sf ~/dotfiles/waybar/network_status.sh ~/.config/waybar/network_status.sh
-ln -sf ~/dotfiles/waybar/usd_rate.sh ~/.config/waybar/usd_rate.sh
-ln -sf ~/dotfiles/waybar/status_scroller_mode.sh ~/.config/waybar/status_scroller_mode.sh
+ln -sf $SCRIPT_DIR/waybar/config ~/.config/waybar/config
+ln -sf $SCRIPT_DIR/waybar/ethernet.sh ~/.config/waybar/ethernet.sh
+ln -sf $SCRIPT_DIR/waybar/modules ~/.config/waybar/modules
+ln -sf $SCRIPT_DIR/waybar/style.css ~/.config/waybar/style.css
+ln -sf $SCRIPT_DIR/waybar/wifi.sh ~/.config/waybar/wifi.sh
+ln -sf $SCRIPT_DIR/waybar/network_status.sh ~/.config/waybar/network_status.sh
+ln -sf $SCRIPT_DIR/waybar/usd_rate.sh ~/.config/waybar/usd_rate.sh
+ln -sf $SCRIPT_DIR/waybar/status_scroller_mode.sh ~/.config/waybar/status_scroller_mode.sh
 
 echo "Create symlinks for dunst."
-ln -sf ~/dotfiles/dunst/dunstrc ~/.config/dunst/dunstrc
+ln -sf $SCRIPT_DIR/dunst/dunstrc ~/.config/dunst/dunstrc
 
 echo "Create symlinks for kitty."
-ln -sf ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -sf ~/dotfiles/kitty/newtab_kitty_choice.sh ~/.config/kitty/newtab_kitty_choice.sh
+ln -sf $SCRIPT_DIR/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf $SCRIPT_DIR/kitty/newtab_kitty_choice.sh ~/.config/kitty/newtab_kitty_choice.sh
 
 echo "Create symlinks for wlogout."
-ln -sf ~/dotfiles/wlogout/layout ~/.config/wlogout
+ln -sf $SCRIPT_DIR/wlogout/layout ~/.config/wlogout
 
 echo "Create symlinks for wofi."
-ln -sf ~/dotfiles/wofi/style.css ~/.config/wofi/style.css
+ln -sf ~$SCRIPT_DIR/wofi/style.css ~/.config/wofi/style.css
 
 echo "Create symlinks for fish."
-ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -sf $SCRIPT_DIR/fish/config.fish ~/.config/fish/config.fish
 
 if grep -q "Jupiter" /sys/devices/virtual/dmi/id/product_name; then
     echo "Create symlinks for opensd"
     rm ~/.config/opensd/config.ini
-    ln -sf ~/dotfiles/steamdeck/opensd/config.ini ~/.config/opensd/config.ini
-    ln -sf ~/dotfiles/steamdeck/opensd/my.profile ~/.config/opensd/profiles/my.profile
+    ln -sf $SCRIPT_DIR/steamdeck/opensd/config.ini ~/.config/opensd/config.ini
+    ln -sf $SCRIPT_DIR/steamdeck/opensd/my.profile ~/.config/opensd/profiles/my.profile
 else
     echo "This is not Jupiter, skipping symlink creation."
 fi
