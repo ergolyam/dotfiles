3d3a75a0b92f3a5ad5a873a78101d44e9a1b611a
Grisha <grisha765@tuta.io>
2025-02-07 02:02:15 +0300

    [u] check battery full

--- dunst/battery_check.sh
+++ dunst/battery_check.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-NOTIFICATION_SENT=0
+LOW_NOTIFICATION_SENT=0
+FULL_NOTIFICATION_SENT=0
 CRITICAL_CAPACITY=15
+FULL_CAPACITY=100
 
 while true; do
     BAT_CAPACITY=$(cat /sys/class/power_supply/BAT*/capacity)
     BAT_STATUS=$(cat /sys/class/power_supply/BAT*/status)
 
     if [[ $BAT_STATUS == "Discharging" && $BAT_CAPACITY -le $CRITICAL_CAPACITY ]]; then
-        if [[ $NOTIFICATION_SENT -eq 0 ]]; then
+        if [[ $LOW_NOTIFICATION_SENT -eq 0 ]]; then
             notify-send -u critical "Warning!" "Battery is low: $BAT_CAPACITY%"
             /usr/bin/paplay $HOME/.config/dunst/battery_low.mp3
-            NOTIFICATION_SENT=1
+            LOW_NOTIFICATION_SENT=1
         fi
     elif [[ $BAT_CAPACITY -gt $CRITICAL_CAPACITY ]]; then
-        NOTIFICATION_SENT=0
+        LOW_NOTIFICATION_SENT=0
     fi
+
+    if [[ $BAT_STATUS == "Full" && $BAT_CAPACITY -eq $FULL_CAPACITY ]]; then
+        if [[ $FULL_NOTIFICATION_SENT -eq 0 ]]; then
+            notify-send -u normal "Battery Full" "Battery is fully charged"
+            /usr/bin/paplay $HOME/.config/dunst/battery_full.ogg
+            FULL_NOTIFICATION_SENT=1
+        fi
+    elif [[ $BAT_CAPACITY -lt $FULL_CAPACITY ]]; then
+      FULL_NOTIFICATION_SENT=0
+    fi
+
     sleep 300
 done
