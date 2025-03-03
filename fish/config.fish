205d7289fb3e63a7a67722072f6db2857ce623e2
Grisha <grisha765@tuta.io>
2025-03-03 05:40:28 +0300

    [u] add loadenv func

--- fish/config.fish
+++ fish/config.fish
@@ -123,4 +123,32 @@ function __postexec --on-event fish_postexec
     set -l duration_ns (math "$end_time - $__cmd_start_time")
     set -g __last_cmd_duration_ms (math "($duration_ns) / 1000000")
 end
+
+function loadenv
+    if test (count $argv) -lt 1
+        echo "Usage: loadenv <env_file>"
+        return 1
+    end
+    set env_file $argv[1]
+    if not test -f $env_file
+        echo "File '$env_file' not found."
+        return 1
+    end
+    while read -l line
+        if test -z "$line"
+            continue
+        end
+        if string match -qr '^#' -- $line
+            continue
+        end
+        set key_val (string split -m 1 '=' -- $line)
+        if test (count $key_val) -ne 2
+            continue
+        end
+        set variable_name $key_val[1]
+        set variable_value (string trim --chars "\"'" -- $key_val[2])
+        set -x -g $variable_name $variable_value
+    end < $env_file
+end
+
 end
