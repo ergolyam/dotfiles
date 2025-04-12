eccc3afacef10efe41be0ac6a1d01811514165bf
Grisha <grisha765@tuta.io>
2025-04-13 02:09:39 +0300

    [+] add fish func symlinks & [r] refactor installer

--- install.sh
+++ install.sh
@@ -1,87 +1,115 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SCRIPT_DIR=$(dirname "$(realpath "$BASH_SOURCE")")
+script_dir=$(dirname "$(realpath "$BASH_SOURCE")")
+base_config="$HOME/.config"
 
-echo "Script dir: $SCRIPT_DIR"
+echo "Script dir: $script_dir"
 
-# Создание необходимых директорий, если они не существуют
 echo "Create dir's."
-mkdir -p ~/.config/hypr
-mkdir -p ~/.config/waybar
-mkdir -p ~/.config/dunst
-mkdir -p ~/.config/kitty
-mkdir -p ~/.config/wlogout
-mkdir -p ~/.config/wofi
-mkdir -p ~/.config/fish
-mkdir -p ~/.config/opensd/profiles
-mkdir -p ~/.local/share/fonts
-
-# Создание символических ссылок для файлов и директорий
+config_dirs=(
+  "hypr"
+  "waybar"
+  "dunst"
+  "kitty"
+  "wlogout"
+  "wofi"
+  "fish"
+  "opensd/profiles"
+  )
+for i in "${config_dirs[@]}"; do
+  mkdir -pv $base_config/${i}
+done
+mkdir -pv ~/.local/share/fonts
 
 echo "Create symlinks for hyprland."
-ln -sf $SCRIPT_DIR/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sf $SCRIPT_DIR/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
-ln -sf $SCRIPT_DIR/hyprland/wallapaper.png ~/.config/hypr/wallapaper.png
-ln -sf $SCRIPT_DIR/hyprland/keys.conf ~/.config/hypr/keys.conf
-ln -sf $SCRIPT_DIR/hyprland/monitors.conf ~/.config/hypr/monitors.conf
-ln -sf $SCRIPT_DIR/hyprland/startup.conf ~/.config/hypr/startup.conf
-ln -sf $SCRIPT_DIR/hyprland/wm.conf ~/.config/hypr/wm.conf
-ln -sf $SCRIPT_DIR/hyprland/wmrules.conf ~/.config/hypr/wmrules.conf
-ln -sf $SCRIPT_DIR/hyprland/plugins ~/.config/hypr/plugins
-ln -sf $SCRIPT_DIR/hyprland/record_screen.sh ~/.config/hypr/record_screen.sh
-ln -sf $SCRIPT_DIR/hyprland/set_workspace.sh ~/.config/hypr/set_workspace.sh
-ln -sf $SCRIPT_DIR/hyprland/toggle_overview_mode.sh ~/.config/hypr/toggle_overview_mode.sh
-ln -sf $SCRIPT_DIR/hyprland/set_scroller_mode.sh ~/.config/hypr/set_scroller_mode.sh
-ln -sf $SCRIPT_DIR/hyprland/screenshot.sh ~/.config/hypr/screenshot.sh
+hypr_files=(
+  "hyprland.conf"
+  "hyprpaper.conf"
+  "wallapaper.png"
+  "keys.conf"
+  "monitors.conf"
+  "startup.conf"
+  "wm.conf"
+  "wmrules.conf"
+  "plugins"
+  "record_screen.sh"
+  "set_workspace.sh"
+  "toggle_overview_mode.sh"
+  "set_scroller_mode.sh"
+  "screenshot.sh"
+  )
+for i in "${hypr_files[@]}"; do
+  ln -sfv $script_dir/hyprland/${i} $base_config/hypr/${i}
+done
 
 echo "Create symlinks for waybar."
-ln -sf $SCRIPT_DIR/waybar/config ~/.config/waybar/config
-ln -sf $SCRIPT_DIR/waybar/ethernet.sh ~/.config/waybar/ethernet.sh
-ln -sf $SCRIPT_DIR/waybar/modules ~/.config/waybar/modules
-ln -sf $SCRIPT_DIR/waybar/style.css ~/.config/waybar/style.css
-ln -sf $SCRIPT_DIR/waybar/wifi.sh ~/.config/waybar/wifi.sh
-ln -sf $SCRIPT_DIR/waybar/network_status.sh ~/.config/waybar/network_status.sh
-ln -sf $SCRIPT_DIR/waybar/usd_rate.sh ~/.config/waybar/usd_rate.sh
-ln -sf $SCRIPT_DIR/waybar/status_scroller_mode.sh ~/.config/waybar/status_scroller_mode.sh
-ln -sf $SCRIPT_DIR/waybar/wttr.sh ~/.config/waybar/wttr.sh
-ln -sf $SCRIPT_DIR/media.sh ~/.config/waybar/media.sh
+waybar_files=(
+  "config"
+  "ethernet.sh"
+  "modules"
+  "style.css"
+  "wifi.sh"
+  "network_status.sh"
+  "usd_rate.sh"
+  "status_scroller_mode.sh"
+  "wttr.sh"
+  "media.sh"
+  )
+for i in "${waybar_files[@]}"; do
+  ln -sfv $script_dir/waybar/${i} $base_config/waybar/${i}
+done
 
 echo "Create symlinks for dunst."
-ln -sf $SCRIPT_DIR/dunst/dunstrc ~/.config/dunst/dunstrc
-ln -sf $SCRIPT_DIR/dunst/battery_check.sh ~/.config/dunst/battery_check.sh
-ln -sf $SCRIPT_DIR/dunst/battery_low.mp3 ~/.config/dunst/battery_low.mp3
-ln -sf $SCRIPT_DIR/dunst/battery_full.ogg ~/.config/dunst/battery_full.ogg
+dunst_files=(
+  "dunstrc"
+  "battery_check.sh"
+  "battery_low.mp3"
+  "battery_full.ogg"
+  )
+for i in "${dunst_files[@]}"; do
+  ln -sfv $script_dir/dunst/${i} $base_config/dunst/${i}
+done
 
 echo "Create symlinks for kitty."
-ln -sf $SCRIPT_DIR/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -sf $SCRIPT_DIR/kitty/newtab_kitty_choice.sh ~/.config/kitty/newtab_kitty_choice.sh
+kitty_files=(
+  "kitty.conf"
+  "newtab_kitty_choice.sh"
+  )
+for i in "${kitty_files[@]}"; do
+  ln -sfv $script_dir/kitty/${i} $base_config/kitty/${i}
+done
 
 echo "Create symlinks for wlogout."
-ln -sf $SCRIPT_DIR/wlogout/layout ~/.config/wlogout
+wlogout_files=(
+  "layout"
+  )
+for i in "${wlogout_files[@]}"; do
+  ln -sfv $script_dir/wlogout/${i} $base_config/wlogout/${i}
+done
 
 echo "Create symlinks for wofi."
-ln -sf $SCRIPT_DIR/wofi/config ~/.config/wofi/config
-ln -sf $SCRIPT_DIR/wofi/style.css ~/.config/wofi/style.css
-ln -sf $SCRIPT_DIR/wofi/wofi_trans.sh ~/.config/wofi/wofi_trans.sh
-ln -sf $SCRIPT_DIR/wofi/wofi_deepl.sh ~/.config/wofi/wofi_deepl.sh
+wofi_files=(
+  "config"
+  "style.css"
+  "wofi_trans.sh"
+  "wofi_deepl.sh"
+  )
+for i in "${wofi_files[@]}"; do
+  ln -sfv $script_dir/wofi/${i} $base_config/wofi/${i}
+done
 
 echo "Create symlinks for fish."
-ln -sf $SCRIPT_DIR/fish/config.fish ~/.config/fish/config.fish
+fish_files=(
+  "config.fish"
+  "functions"
+  )
+for i in "${fish_files[@]}"; do
+  ln -sfv $script_dir/fish/${i} $base_config/fish/${i}
+done
 
 echo "Create fonts symlinks"
-ln -sf $SCRIPT_DIR/fonts/TerminessNerdFont-Regular.ttf ~/.local/share/fonts/TerminessNerdFont-Regular.ttf
-ln -sf $SCRIPT_DIR/fonts/FiraCodeNerdFontMono-Regular.ttf ~/.local/share/fonts/FiraCodeNerdFontMono-Regular.ttf
-
-if grep -q "Jupiter" /sys/devices/virtual/dmi/id/product_name; then
-    echo "Create symlinks for jupiter"
-    rm ~/.config/opensd/config.ini
-    ln -sf $SCRIPT_DIR/steamdeck/opensd/config.ini ~/.config/opensd/config.ini
-    ln -sf $SCRIPT_DIR/steamdeck/opensd/my.profile ~/.config/opensd/profiles/my.profile
-    ln -sf $SCRIPT_DIR/hyprland/monitors_select.sh ~/.config/hypr/monitors_select.sh
-    sed -i '/^#exec-once = .*monitors_select\.sh &/ s/^#//; /^#exec-once = .*opensdd &/ s/^#//' $SCRIPT_DIR/hyprland/startup.conf
-else
-    echo "This is not Jupiter, skipping symlink creation."
-fi
+ln -sfv $script_dir/fonts/TerminessNerdFont-Regular.ttf ~/.local/share/fonts/TerminessNerdFont-Regular.ttf
+ln -sfv $script_dir/fonts/FiraCodeNerdFontMono-Regular.ttf ~/.local/share/fonts/FiraCodeNerdFontMono-Regular.ttf
 
 echo "Dotfiles have been linked."
 
