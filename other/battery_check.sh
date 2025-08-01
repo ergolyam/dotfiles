74d4634ef54c63cb3d39ffca6518389575f5ce42
Grisha <grisha765@tuta.io>
2025-08-01 17:30:44 +0300

    [u] using ac status

--- other/battery_check.sh
+++ other/battery_check.sh
@@ -3,13 +3,13 @@
 LOW_NOTIFICATION_SENT=0
 FULL_NOTIFICATION_SENT=0
 CRITICAL_CAPACITY=15
-FULL_CAPACITY=100
+FULL_CAPACITY=75
 
 while true; do
     BAT_CAPACITY=$(cat /sys/class/power_supply/BAT*/capacity)
-    BAT_STATUS=$(cat /sys/class/power_supply/BAT*/status)
+    AC_ONLINE=$(cat /sys/class/power_supply/AC*/online)
 
-    if [[ $BAT_STATUS == "Discharging" && $BAT_CAPACITY -le $CRITICAL_CAPACITY ]]; then
+    if [[ $AC_ONLINE -eq 0 && $BAT_CAPACITY -le $CRITICAL_CAPACITY ]]; then
         if [[ $LOW_NOTIFICATION_SENT -eq 0 ]]; then
             notify-send -u critical "Warning!" "Battery is low: $BAT_CAPACITY%"
             /usr/bin/paplay $HOME/.local/share/other/battery_low.mp3
@@ -19,7 +19,7 @@ while true; do
         LOW_NOTIFICATION_SENT=0
     fi
 
-    if [[ $BAT_STATUS == "Full" && $BAT_CAPACITY -eq $FULL_CAPACITY ]]; then
+    if [[ $AC_ONLINE -eq 1 && $BAT_CAPACITY -eq $FULL_CAPACITY ]]; then
         if [[ $FULL_NOTIFICATION_SENT -eq 0 ]]; then
             notify-send -u normal "Battery Full" "Battery is fully charged"
             /usr/bin/paplay $HOME/.local/share/other/battery_full.ogg
